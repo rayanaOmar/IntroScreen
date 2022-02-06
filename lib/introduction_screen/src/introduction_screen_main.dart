57de9651ca10044213654a04d62683a68db427ab
rayanaOmar <rayanaomar_@hotmail.com>
2022-02-06 08:16:00 +0300

    intro_screen_update

---
+++ lib/introduction_screen/src/introduction_screen_main.dart
@@ -0,0 +1,195 @@
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:intro_screen/introduction_screen/methods/colors.dart';
+import 'package:intro_screen/introduction_screen/src/introduction_screen.dart';
+import 'package:intro_screen/introduction_screen/src/model/page_decoration.dart';
+import 'package:intro_screen/introduction_screen/src/model/page_view_model.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final introKey = GlobalKey<IntroductionScreenState>();
+
+  void _onIntroEnd(context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) => const MainPage()),
+    );
+  }
+
+  Widget _buildFullscreenImage(String assetName) {
+    return Image.asset(
+      'lib/introduction_screen/assets/$assetName',
+      fit: BoxFit.cover,
+      height: double.infinity,
+      width: double.infinity,
+      alignment: Alignment.center,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const bodyStyle = TextStyle(fontSize: 19.0);
+
+    const pageDecoration = PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      key: introKey,
+      globalBackgroundColor: kPink,
+      globalHeader: const Align(
+        alignment: Alignment.topRight,
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(top: 10, right: 10),
+          ),
+        ),
+      ),
+      //------------------------------------------
+      //Pages Section
+      pages: [
+        PageViewModel(
+            title: "تواصل بالفيديو",
+            body: "يمكنك الان التواصل مع المشاهير والمؤثرين عن طريق الفيديو, يمكنك الان التواصل مع المشاهير والمؤثرين عن طريق الفيديو",
+            image: _buildFullscreenImage("intro1.png"),
+            decoration: pageDecoration.copyWith(
+              titleTextStyle: const TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontFamily: 'Cairo'),
+              bodyTextStyle: const TextStyle(
+                  fontSize: 13,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.white70,
+                  fontFamily: 'Cairo'
+              ),
+              contentMargin: const EdgeInsets.symmetric(horizontal: 16),
+              fullScreen: true,
+              bodyFlex: 2,
+              imageFlex: 3,
+            )),
+        PageViewModel(
+            title: "نجمك المفضل عندنا",
+            body: "يمكنك الان التواصل مع المشاهير والمؤثرين عن طريق الفيديو, يمكنك الان التواصل مع المشاهير والمؤثرين عن طريق الفيديو",
+            image: _buildFullscreenImage("intro2.png"),
+            decoration: pageDecoration.copyWith(
+              titleTextStyle: const TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontFamily: 'Cairo'),
+              bodyTextStyle: const TextStyle(
+                  fontSize: 13,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.white70,
+                  fontFamily: 'Cairo'
+              ),
+              contentMargin: const EdgeInsets.symmetric(horizontal: 16),
+              fullScreen: true,
+              bodyFlex: 2,
+              imageFlex: 3,
+            )),
+        PageViewModel(
+            title: "المطربين متواجدين",
+            body: "يمكنك الان التواصل مع المشاهير والمؤثرين عن طريق الفيديو, يمكنك الان التواصل مع المشاهير والمؤثرين عن طريق الفيديو",
+            image: _buildFullscreenImage("intro3.png"),
+            decoration: pageDecoration.copyWith(
+              titleTextStyle: const TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontFamily: 'Cairo'),
+              bodyTextStyle: const TextStyle(
+                  fontSize: 13,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.white70,
+                  fontFamily: 'Cairo'
+              ),
+              contentMargin: const EdgeInsets.symmetric(horizontal: 16),
+              fullScreen: true,
+              bodyFlex: 2,
+              imageFlex: 3,
+            )),
+        PageViewModel(
+            title: "ابدأ تجربتك الان",
+            body: "يمكنك الان التواصل مع المشاهير والمؤثرين عن طريق الفيديو, يمكنك الان التواصل مع المشاهير والمؤثرين عن طريق الفيديو",
+            image: _buildFullscreenImage("intro4.png"),
+            decoration: pageDecoration.copyWith(
+              titleTextStyle: const TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontFamily: 'Cairo'),
+              bodyTextStyle: const TextStyle(
+                  fontSize: 13,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.white70,
+                  fontFamily: 'Cairo'
+              ),
+              contentMargin: const EdgeInsets.symmetric(horizontal: 16),
+              fullScreen: true,
+              bodyFlex: 2,
+              imageFlex: 3,
+            )),
+      ],
+      onDone: () => _onIntroEnd(context),
+      onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      showSkipButton: true,
+      skipOrBackFlex: 0,
+      nextFlex: 0,
+      showBackButton: false,
+
+      skip: const Text(
+        'تخطي',
+        style: TextStyle(fontWeight: FontWeight.w600),
+      ),
+      done: const Text('بدء', style: TextStyle(fontWeight: FontWeight.w600)),
+      curve: Curves.fastLinearToSlowEaseIn,
+      controlsMargin: const EdgeInsets.all(16),
+      controlsPadding: kIsWeb
+          ? const EdgeInsets.all(12.0)
+          : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+      dotsContainerDecorator: const ShapeDecoration(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        ),
+      ),
+    );
+  }
+}
+
+//----------------------------------------------------------------
+//Main Screen
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main Screen'),
+        backgroundColor: kPink,
+      ),
+      body: const Center(
+          child: Text("This is the screen after Introduction Screens")),
+    );
+  }
+}
